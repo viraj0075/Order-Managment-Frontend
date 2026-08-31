ada959cac7460f024fc1708d2223c0fb22a74aca
viraj0075 <viraj.koradia.learn.7979@gmail.com>
2026-08-31 13:16:37 +0530

    feat: implement responsive footer component with navigation and subscription form

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -168,6 +168,7 @@ const Footer = () => {
                             <div className="flex gap-2.5">
                                 {footerLinks.social.map((social, idx) => (
                                     <a
+                                        key={idx}
                                         href={social.href}
                                         target="_blank"
                                         rel="noopener noreferrer"
@@ -180,7 +181,7 @@ const Footer = () => {
                             </div>
                         </div>
                         <p className="text-gray-400 text-xs font-semibold select-none">
-                            © {new Date().getFllYear()} Snacks N Snacks. Build by Viraj Koradia❤️.
+                            © {new Date().getFullYear()} Snacks N Snacks. Build by Viraj Koradia❤️.
                         </p>
                     </div>
 
